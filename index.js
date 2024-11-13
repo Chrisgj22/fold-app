3b0dfa959a2039e58584f95d80d3bbbfc38cabb9
Chrisgj22 <121605828+Chrisgj22@users.noreply.github.com>
2024-11-13 19:15:47 +1100

    1.0

--- index.js
+++ index.js
@@ -10,10 +10,15 @@ btn.addEventListener("click", function(){
     let thick = thickness.value*2
     let wDimensions= width.value -  thick + (depth.value*2) - thick
     let hDimension= height.value - thick + (depth.value*2) - thick
+    let dVal = depth.value - thickness.value
     result.innerHTML=`<div class="resDiv">
                     <p>Cutting size:</p>
-                     <div class="Perfect"><p>Perfect fit</p> ${wDimensions} x ${hDimension}</div>
-                     <div class="safe"><p>Safe fit (-1mm allowance)</p> ${wDimensions - 1} x ${hDimension - 1}</div>
+                    <div class="Perfect"><p>Perfect fit</p> ${wDimensions} x ${hDimension}</div>
+                    <div>notch:${dVal}</div>
+                    <div class="safe">Brake-press backstop ${dVal}</div>
+                    <div class="safe"><p>Safe fit (-1mm allowance)</p> ${wDimensions - 1} x ${hDimension - 1}</div>
+                    <div class="safe">notch: ${dVal}</div>
+                    <div>Brake-press backstop ${dVal}</div>
                     </div>`
     console.log("click")
     
